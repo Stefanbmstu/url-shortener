3756e1e5ff11cbf7b29694130f112fdfaa59cf74
Stefan <stefan@MacBook-Pro-Stefan.local>
2025-09-19 16:54:45 +0300

    Random alias

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ toolchain go1.24.7
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/ajg/form v1.5.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.18.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-chi/chi/v5 v5.2.3 // indirect
@@ -20,6 +21,8 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-sqlite3 v1.14.32 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/exp v0.0.0-20250911091902-df9299821621 // indirect
 	golang.org/x/net v0.34.0 // indirect
--- internal/http-server/handlers/url/save/save.go
+++ internal/http-server/handlers/url/save/save.go
@@ -5,13 +5,15 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+	"golang.org/x/exp/slog"
+
 	resp "url-shortener/internal/lib/api/response"
 	"url-shortener/internal/lib/logger/sl"
-
-	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/render"
-	"github.com/go-playground/validator"
-	"golang.org/x/exp/slog"
+	"url-shortener/internal/lib/random"
+	"url-shortener/internal/storage"
 )
 
 type Request struct {
@@ -24,11 +26,14 @@ type Response struct {
 	Alias string `json:"alias,omitempty"`
 }
 
+// TODO: move to config if needed
+const aliasLength = 6
+
 type URLSaver interface {
-	Save(urlToSave string, alias string) (int64, error)
+	SaveURL(urlToSave string, alias string) (int64, error)
 }
 
-func New(log *slog.Logger, storage URLSaver) http.HandlerFunc {
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		const op = "handlers.url.save.New"
 
@@ -68,5 +73,30 @@ func New(log *slog.Logger, storage URLSaver) http.HandlerFunc {
 
 			return
 		}
+
+		alias := req.Alias
+		if alias == "" {
+			alias = random.NewRandomString(aliasLength)
+		}
+
+		id, err := urlSaver.SaveURL(req.URL, alias)
+		if errors.Is(err, storage.ErrURLExists) {
+			log.Info("url already exists", slog.String("url", req.URL))
+
+			render.JSON(w, r, resp.Error("url already exists"))
+
+			return
+		}
+		if err != nil {
+			log.Error("failed to add url", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to add url"))
+
+			return
+		}
+
+		log.Info("url added", slog.Int64("id", id))
+
+		// responseOK(w, r, alias)
 	}
 }
