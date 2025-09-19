67fbd2fdd8f1ef53b84055b37d31a873aa58067d
Stefan <stefan@MacBook-Pro-Stefan.local>
2025-09-19 17:07:03 +0300

    Started server

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"url-shortener/internal/config"
@@ -11,6 +12,7 @@ import (
 
 	"golang.org/x/exp/slog"
 
+	"url-shortener/internal/http-server/handlers/url/save"
 	mwLogger "url-shortener/internal/http-server/middleware/logger"
 
 	"github.com/go-chi/chi/v5"
@@ -50,6 +52,23 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
 
+	router.Post("/url", save.New(log, storage))
+
+	log.Info("starting http server", slog.String("address", cfg.Address))
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start http server")
+	}
+
+	log.Error("server stopped")
 	// TODO: run server
 
 }
